9189fee19fdba9cfb1191019edc9f38e9530b472
nohaemad123 <noha2697@gmail.com>
2024-08-12 15:35:05 +0300

    first commit

---
+++ js/plugins.js
@@ -0,0 +1,14 @@
+/*global $, alert, document, eq */
+
+$(function () {
+    'use strict';
+    
+            $("html").niceScroll();
+ $(' .carousel').carousel({
+     interval: 1000  
+    });
+});
+
+
+
+
